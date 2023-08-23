fbd1367800ca0800246e8076630a3222c2ff2e00
pan <2556323541@qq.com>
2023-08-24 00:38:23 +0800

    fe: fix ResizeObserver bug

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,27 @@
 <script>
 import NavBar from './components/NavBar.vue'
 
+const debounce = (fn, delay) => {
+  let timer = null;
+  return function () {
+    let context = this;
+    let args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
+  }
+}
+
+const _ResizeObserver = window.ResizeObserver;
+window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+  constructor(callback) {
+    callback = debounce(callback, 16);
+    super(callback);
+  }
+}
+
+
 export default {
     name: 'app',
     components: {
